5f2955e67231301d4c5e6e30ad1e4b0fc3030c7d
remi95 <remi.mafat@ynov.com>
2018-04-08 19:31:45 +0200

    add secret_config file to configure

---
+++ configure_secret_config.py
@@ -0,0 +1,13 @@
+# Rename this file secret_config.py and change the values
+
+DATABASE_HOST = 'localhost'
+DATABASE_USER = 'your-database-username'
+DATABASE_PASSWORD = 'your-database-password'
+DATABASE_NAME = 'http_status'
+SECRET_KEY = 'your-secret-key'
+
+SLACK_TOKEN = 'your-slack-bot-token'
+SLACK_CHANNEL = 'your-slack-channel-id'
+
+TELEGRAM_TOKEN = 'your-telegram-bot-token'
+TELEGRAM_CHAT_ID = 'your-telegram-chat-id'
